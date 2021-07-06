e7aa0ca76903c1ae6e39536ae07c9db43f10ed5c
Perlowski <perlowskistephen@gmail.com>
2021-07-06 17:58:07 -0500

    starter page

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+var questionsEl = document.querySelector("#questions");
+var timerEl = document.querySelector("#time");
+var choicesEl = document.querySelector("#choices");
+var submitBtn = document.querySelector("#submit");
+var startBtn = document.querySelector("#start");
+var initialsEl = document.querySelector("#initials");
+var feedbackEl = document.querySelector("#feedback");
