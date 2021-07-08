3ca84901c707e05f7d471bc10ce6fb8dd88ef50b
Perlowski <perlowskistephen@gmail.com>
2021-07-08 10:32:30 -0500

    counter attempt

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,16 @@
-var questionsEl = document.querySelector("#questions");
-var timerEl = document.querySelector("#time");
-var choicesEl = document.querySelector("#choices");
-var submitBtn = document.querySelector("#submit");
-var startBtn = document.querySelector("#start");
-var initialsEl = document.querySelector("#initials");
-var feedbackEl = document.querySelector("#feedback");
+var buttonEl = document.querySelector("#save");
+var counter = document.querySelector("#time-remaining").textContent;
+
+var countdown = function () {
+  console.log(counter);
+  counter--;
+  if (counter === 0) {
+    alert("You Lose! Good day sir...I said GOOD DAY!");
+    clearInterval(startCountdown);
+  }
+};
+
+buttonEl.addEventListener("click", function () {
+  alert("button clicked");
+  var countdown = setInterval(countdown, 1000);
+});
