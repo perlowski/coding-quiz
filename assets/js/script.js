820302a6ff1ccd1a39306da5755d9fc4f44eb145
Perlowski <perlowskistephen@gmail.com>
2021-07-09 11:52:30 -0500

    questions working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,49 @@
+var questions = [
+  {
+    title: "Commonly used data types do NOT include:",
+    choices: ["Alerts", "strings", "numbers", "booleans"],
+    answer: "Alerts",
+  },
+  {
+    title: "The condition in an if/else statement is enclosed with ________.",
+    choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+    answer: "parenthesis",
+  },
+  {
+    title: "Arrays in JavaScript can be used to store ________.",
+    choices: [
+      "numbers & strings",
+      "other arrays",
+      "booleans",
+      "all of the above",
+    ],
+    answer: "all of the above",
+  },
+  {
+    title:
+      "String values must be enclosed within ________ when being assigned to variables",
+    choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+    answer: "quotes",
+  },
+  {
+    title:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+    answer: "console.log",
+  },
+];
+var currentQuestionIndex = 0;
+var questionsEl = document.querySelector("#qs");
+var choicesEl = document.querySelector("#choices");
+var checkEl = document.querySelector("#check");
 var buttonEl = document.querySelector("#save");
 var counter = document.querySelector("#time-remaining").textContent;
 var countdown;
 
 buttonEl.addEventListener("click", function () {
-  questionOne();
+  var startScreenEl = document.getElementById("prequiz");
+  startScreenEl.setAttribute("class", "hide");
+  startQuestions();
   var countdown = setInterval(function () {
     counter--;
     if (counter < 0) {
@@ -19,12 +59,36 @@ buttonEl.addEventListener("click", function () {
   }, 1000);
 });
 
-var questionOne = function () {
-  var q1Page = document.querySelector("#prequiz");
-  var q1Q = document.createElement("h2");
-  q1Q.textContent =
-    "What programing language represents the skin of a webpage?";
-  q1Q.className = "start-page";
-  q1Page.appendChild(q1Q);
-  document.getElementById("prequiz").innerHTML = q1Page;
+var startQuestions = function () {
+  var currentQuestion = questions[currentQuestionIndex];
+  var titleEl = document.getElementById("question-title");
+  titleEl.setAttribute("class", "start-page");
+  titleEl.textContent = currentQuestion.title;
+  choicesEl.innerHTML = "";
+  currentQuestion.choices.forEach(function (choice, i) {
+    var choiceNode = document.createElement("button");
+    choiceNode.setAttribute("class", "choiceStyle");
+    choiceNode.setAttribute("value", choice);
+    choiceNode.textContent = choice;
+    choiceNode.onclick = choiceClick;
+    choicesEl.appendChild(choiceNode);
+  });
+};
+
+var choiceClick = function () {
+  // if wrong
+  if (this.value !== questions[currentQuestionIndex].answer) {
+    counter -= 15;
+    checkEl.textContent = "wrong";
+    //if right
+  } else {
+    checkEl.textContent = "correct";
+  }
+  currentQuestionIndex++;
+
+  if (currentQuestionIndex === questions.length) {
+    alert("GAME OVER");
+  } else {
+    startQuestions();
+  }
 };
